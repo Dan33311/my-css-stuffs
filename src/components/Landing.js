5c0bfde3ecf68dc33c12db2378d8349e699a504c
dan33311 <dansaro.groger@gmail.com>
2023-03-23 17:19:52 -0500

    added a 2nd animation

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,17 +1,30 @@
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/Landing.module.css'
 
 export default function Landing() {
   return (
     <>
       <div className={styles.landing}>
         <h1 className={styles.title}>My CSS Animations</h1>
-
-        <div className={styles.animation_container}>
-          <div className={styles.box}>
-            <span className={`${styles.span} ${styles.image_1} ${styles.animado2}`}></span>
+        
+        <div className={styles.container}>
+          
+          {/* animation-1 on hover */}
+          <div className={styles.animation_container}>
+            <h3 className={styles.title}>Animation 1</h3>
+            <div className={styles.box}>
+              <span className={`${styles.span} ${styles.image_1}`}></span>
+            </div>
           </div>
-        </div>
 
+          {/* animation 2  */}
+          <div className={styles.animation_container}>
+            <h3 className={styles.title}>Animation 2 - GLITCH</h3>
+            <div className={styles.testb}>
+              <img className={`${styles.image_2} ${styles.glitch_animation}`} src="/rl-red.png" alt="" />
+            </div>
+          </div>
+
+        </div>
       </div>
     </>
   )
